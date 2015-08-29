0d9523f702d4a6c8d3f42ece3febd514b4a3d3b7
Roman Roibu <roman.roibu@gmail.com>
2015-08-29 16:18:52 +0300

    Add empty project :space_invader:

---
+++ WolframAlpha/WolframAlpha.h
@@ -0,0 +1,19 @@
+//
+//  WolframAlpha.h
+//  WolframAlpha
+//
+//  Created by Roman Roibu on 8/29/15.
+//  Copyright © 2015 Roman Roibu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WolframAlpha.
+FOUNDATION_EXPORT double WolframAlphaVersionNumber;
+
+//! Project version string for WolframAlpha.
+FOUNDATION_EXPORT const unsigned char WolframAlphaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WolframAlpha/PublicHeader.h>
+
+
